8213e6782e318ff51fc37d3c69fa105032272fa9
Jake Unsted <jake.unsted@gmail.com>
2025-02-19 19:13:12 +0000

    init nuxt and capacitor

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mealdiary.app',
+  appName: 'meal-diary',
+  webDir: 'www'
+};
+
+export default config;
