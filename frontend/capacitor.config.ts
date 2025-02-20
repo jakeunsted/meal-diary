54f543208249d129d199c02d7e9dd404ec5593bb
Jake Unsted <jake.unsted@gmail.com>
2025-02-20 19:08:34 +0000

    tailwind and daisyui init

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.mealdiary.app',
   appName: 'meal-diary',
-  webDir: 'www'
+  webDir: 'dist'
 };
 
 export default config;
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseKey: process.env.SUPABASE_KEY
+    }
+  },
+  css: ['@/assets/css/tailwind.css'],
+  postcss: {
+    plugins: {
+      tailwindcss,
+    },
+  },
 })
