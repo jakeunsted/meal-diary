7d1146e7fe0cbe8298a1958e1db298eccf55e9b7
Jake Unsted <jake.unsted@melodimedia.co.uk>
2025-02-27 15:07:47 +0000

    init profile

--- frontend/pages/profile/index.vue
+++ frontend/pages/profile/index.vue
@@ -1,22 +1,71 @@
 <template>
   <div>
-    <h1 class="text-2xl font-bold text-center m-4">
-      Your Profile
-    </h1>
-
-    <div>
+    <div class="mt-4">
       <div class="avatar flex justify-center">
         <div class="mask mask-squircle w-24">
-          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+          <img :src="user.avatar" />
         </div>
       </div>
-      <!-- divider -->
+      <h2 class="text-center font-bold">{{ user.name }}</h2>
+
       <div class="divider mx-4"></div>
+
+      <div class="card bg-base-200 m-4 shadow-sm ">
+        <div class="card-title px-6 my-2">
+          <h2 class="font-bold">Your family members</h2>
+        </div>
+        <div class="bg-base-300 rounded-b-lg pb-6">
+          <div class="card-body">
+            <div class="flex flex-col gap-4">
+              <div class="flex flex-row gap-4">
+                <div v-for="member in familyMembers" :key="member.name">
+                  <div class="flex flex-col gap-2">
+                    <div class="avatar justify-center">
+                      <div class="w-12 rounded-full">
+                        <img :src="member.avatar" />
+                      </div>
+                    </div>
+                    <h3 class="font-semibold">{{ member.name }}</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card-actions px-6">
+            <button 
+              class="btn btn-primary"
+              @click="addFamilyMember"
+            >Add family member</button>
+          </div>
+        </div>
+      </div>
     </div>
 
   </div>
 </template>
 
 <script setup lang="ts">
+const user = ref({
+  id: 1,
+  name: 'Dave Doe',
+  avatar: '/temp-avatars/avataaars1.png',
+});
 
+const familyMembers = ref([
+  {
+    id: 1,
+    name: 'John Doe',
+    avatar: '/temp-avatars/avataaars2.png',
+  },
+  {
+    id: 2,
+    name: 'Jane Doe',
+    avatar: '/temp-avatars/avataaars3.png',
+  },
+  
+]);
+
+const addFamilyMember = () => {
+  console.log('addFamilyMember');
+};
 </script>
--- frontend/pages/shopping-list.vue
+++ frontend/pages/shopping-list.vue
@@ -3,7 +3,11 @@
     <h1 class="text-2xl font-bold text-center m-4">
       Shopping List
     </h1>
-    <CollapseListSection class="mx-4" categoryTitle="Fruits" :categoryItems="dummyFruits" />
+    <div>
+      <div v-for="category in shoppingCategories" :key="category.id">
+        <CollapseListSection class="mx-4" :categoryTitle="category.name" :categoryItems="category.items" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,5 +19,35 @@ const dummyFruits = [
   { id: 2, name: 'Banana' },
   { id: 3, name: 'Cherry' },
 ];
+
+const dummyMeats = [
+  { id: 1, name: 'Beef' },
+  { id: 2, name: 'Chicken' },
+  { id: 3, name: 'Fish' },
+];
+
+const dummyBaking = [
+  { id: 1, name: 'Bread' },
+  { id: 2, name: 'Cake' },
+  { id: 3, name: 'Pastry' },
+];
+
+const dummyAlcohol = [
+  { id: 1, name: 'Beer' },
+  { id: 2, name: 'Wine' },
+  { id: 3, name: 'Spirits' },
+];
+
+const dummyOther = [
+  { id: 1, name: 'Sponges' },
+];
+
+const shoppingCategories = ref([
+  { id: 1, name: 'Fruits & Vegetables', items: dummyFruits },
+  { id: 2, name: 'Meat & Fish', items: dummyMeats },
+  { id: 3, name: 'Baking', items: dummyBaking },
+  { id: 4, name: 'Alcohol', items: dummyAlcohol },
+  { id: 5, name: 'Other', items: dummyOther },
+]);
 
 </script>
