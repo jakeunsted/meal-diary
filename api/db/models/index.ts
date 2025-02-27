a95011dbd5720c7528513a9c7e8d349562056930
Jake Unsted <jake.unsted@melodimedia.co.uk>
2025-02-27 17:33:33 +0000

    db port secret

--- api/db/models/index.ts
+++ api/db/models/index.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-if (!process.env.DB_NAME || !process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_HOST) {
+if (!process.env.DB_NAME || !process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_HOST || !process.env.DB_PORT) {
   throw new Error('Database configuration is missing');
 }
 
@@ -13,6 +13,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
     dialect: 'postgres',
     logging: process.env.NODE_ENV === 'development' ? console.log : false,
   }
