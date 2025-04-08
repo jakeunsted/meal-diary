8637aac11523bd0e739ab2e956927a5e55dd2213
Jake Unsted <jake.unsted@gmail.com>
2025-04-08 20:54:53 +0100

    prod db on rds

--- api/db/models/index.ts
+++ api/db/models/index.ts
@@ -19,14 +19,14 @@ if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
   dbHost = process.env.DEV_DB_HOST;
   dbPort = process.env.DEV_DB_PORT;
 } else {
-  if (!process.env.DB_NAME || !process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_HOST || !process.env.DB_PORT) {
+  if (!process.env.PROD_DB_NAME || !process.env.PROD_DB_USER || !process.env.PROD_DB_PASSWORD || !process.env.PROD_DB_HOST || !process.env.PROD_DB_PORT) {
     throw new Error('Database configuration is missing');
   }
-  dbName = process.env.DB_NAME;
-  dbUser = process.env.DB_USER;
-  dbPassword = process.env.DB_PASSWORD;
-  dbHost = process.env.DB_HOST;
-  dbPort = process.env.DB_PORT;
+  dbName = process.env.PROD_DB_NAME;
+  dbUser = process.env.PROD_DB_USER;
+  dbPassword = process.env.PROD_DB_PASSWORD;
+  dbHost = process.env.PROD_DB_HOST;
+  dbPort = process.env.PROD_DB_PORT;
 }
 
 const sequelize = new Sequelize(
@@ -37,6 +37,12 @@ const sequelize = new Sequelize(
     host: dbHost,
     port: parseInt(dbPort),
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    },
     logging: process.env.NODE_ENV === 'development' ? console.log : false,
   }
 );
