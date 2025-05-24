9c9eec2057cc7b9e6a3145b46345f70aeb9deffe
Jake Unsted <jake.unsted@gmail.com>
2025-05-24 09:00:26 +0100

    capacitor settings

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.mealdiary.app',
   appName: 'meal-diary',
-  webDir: 'dist'
+  webDir: '.output/public',
+  server: {
+    // Use 10.0.2.2 to access host machine's localhost from Android emulator
+    url: process.env.NODE_ENV === 'development' ? 'http://10.0.2.2:3000' : 'https://mealdiary.co.uk',
+    cleartext: true
+  }
 };
 
 export default config;
