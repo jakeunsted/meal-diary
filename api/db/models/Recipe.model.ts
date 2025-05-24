5f0e3fc4ea8a76ea4f554de9f5e8777a2c2bd453
Jake Unsted <jake.unsted@gmail.com>
2025-05-24 21:23:37 +0100

    Add family group to recipe

--- api/db/models/Recipe.model.ts
+++ api/db/models/Recipe.model.ts
@@ -1,19 +1,24 @@
 import { DataTypes, Model } from 'sequelize';
 import type { Optional } from 'sequelize';
 import sequelize from './index.ts';
+import FamilyGroup from './FamilyGroup.model.ts';
 
 export interface RecipeAttributes {
   id: number;
   name: string;
   method: string;
   created_by: number;
+  family_group_id?: number;
   created_at?: Date;
   updated_at?: Date;
 }
 
-interface RecipeCreationAttributes extends Optional<RecipeAttributes, 'id' | 'created_at' | 'updated_at'> {}
+interface RecipeCreationAttributes extends Optional<RecipeAttributes, 'id' | 'created_at' | 'updated_at' | 'family_group_id'> {}
 
-class Recipe extends Model<RecipeAttributes, RecipeCreationAttributes> {}
+class Recipe extends Model<RecipeAttributes, RecipeCreationAttributes> {
+  public getFamilyGroup!: () => Promise<FamilyGroup>;
+  public setFamilyGroup!: (familyGroup: FamilyGroup) => Promise<void>;
+}
 
 Recipe.init(
   {
@@ -33,6 +38,14 @@ Recipe.init(
     created_by: {
       type: DataTypes.INTEGER,
       allowNull: false,
+    },
+    family_group_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'family_groups',
+        key: 'id',
+      },
     },
     created_at: {
       type: DataTypes.DATE,
