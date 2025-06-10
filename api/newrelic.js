86ea18ef9072b67954b2bb193050e7f3cf99babe
Jake Unsted <jake.unsted@melodimedia.co.uk>
2025-06-10 16:16:38 +0100

    newrelic log level

--- api/newrelic.js
+++ api/newrelic.js
@@ -20,7 +20,7 @@ exports.config = {
      * issues with the agent, 'info' and higher will impose the least overhead on
      * production applications.
      */
-    level: 'info'
+    level: process.env.NEW_RELIC_LOG_LEVEL || 'trace'
   },
   /**
    * When true, all request headers except for those listed in attributes.exclude
